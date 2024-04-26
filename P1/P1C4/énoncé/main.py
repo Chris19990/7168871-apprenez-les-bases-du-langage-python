828a3d8ace67b01bb52ed855861334d8766a4e7e
Sunday <skogbetse@gmail.com>
2024-04-26 11:31:36 +0000

    exercice traité

--- P1/P1C4/énoncé/main.py
+++ P1/P1C4/énoncé/main.py
@@ -1,1 +1,5 @@
-# Écrivez votre code ici !
+# Écrivez votre code ici !# Écrivez votre code ici !nom = "Sunday"
+age = 23
+print(f"Je m'appelle {nom} et j'ai {age} ans.")
+AGE = age + 10
+print(f"je m'appelle {nom} et j'ai {AGE} ans.")
