ab4e8bdd96f548aea3894a00a6c0cebe9cbf51f3
Sunday <skogbetse@gmail.com>
2024-04-26 12:03:11 +0000

    modification

--- P1/P1C4/énoncé/main.py
+++ P1/P1C4/énoncé/main.py
@@ -1,5 +1,5 @@
-# Écrivez votre code ici !# Écrivez votre code ici !nom = "Sunday"
+# Écrivez votre code ici !nom = "Sunday"
 age = 23
 print(f"Je m'appelle {nom} et j'ai {age} ans.")
 AGE = age + 10
-print(f"je m'appelle {nom} et j'ai {AGE} ans.")
+print(f"je m'appelle {nom} et j'ai {AGE} ans maintenant.")
