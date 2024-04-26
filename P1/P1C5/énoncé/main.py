f67365c329dbe24297c6c086c6228c5fbfc065d7
Sunday <skogbetse@gmail.com>
2024-04-26 13:12:18 +0000

    session 2

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,14 @@
-# Écrivez votre code ici !
+nom = "Sunday"
+age = int(23)
+taille = float(1.63)
+est_etudiant = 'True'
+
+print("nom:", nom)
+print("age:" , age )
+print("taille:", taille)
+print ("est_etudiant:", est_etudiant)
+
+print ("type nom:",(type(nom)))
+print("type age:",(type(age)))
+print("type taille:", (type(taille)))
+print("type est_etudiant:", (type(est_etudiant)))
